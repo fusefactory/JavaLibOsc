05625cf89ca23ae7ce5b6bd6a88dcd50656d0cfd
paolofuse <paolo@fusefactory.it>
2017-06-13 10:31:13 +0200

    Fixed bug when socket is closed.

--- src/fuse/osc/OSCReceiver.java
+++ src/fuse/osc/OSCReceiver.java
@@ -7,62 +7,51 @@ import java.net.SocketException;
 
 import fuse.osc.utils.OSCByteArrayToJavaConverter;
 
-public class OSCReceiver extends OSCPort implements Runnable
-{
+public class OSCReceiver extends OSCPort implements Runnable {
 	private boolean isListening;
 	private OSCByteArrayToJavaConverter converter;
 	private OSCPacketDispatcher dispatcher;
-	
-	public OSCReceiver(int port) throws SocketException
-	{
+
+	public OSCReceiver(int port) throws SocketException {
 		this.port = port;
 		socket = new DatagramSocket(port);
 		converter = new OSCByteArrayToJavaConverter();
 		dispatcher = new OSCPacketDispatcher();
 	}
-	
-	public void startListening()
-	{
+
+	public void startListening() {
 		isListening = true;
 		Thread thread = new Thread(this);
 		thread.start();
 	}
-	
-	public void stopListening()
-	{
+
+	public void stopListening() {
 		isListening = false;
 	}
-	
-	public boolean isListening()
-	{
+
+	public boolean isListening() {
 		return isListening;
 	}
-	
-	public void addListener(OSCListener listener)
-	{
+
+	public void addListener(OSCListener listener) {
 		dispatcher.addListener(listener);
 	}
-	
-	public void removeListener(OSCListener listener)
-	{
+
+	public void removeListener(OSCListener listener) {
 		dispatcher.removeListener(listener);
 	}
-	
+
 	@Override
-	public void run()
-	{
+	public void run() {
 		byte[] buffer = new byte[1536];
 		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-		while (isListening)
-		{
-			try
-			{
-				socket.receive(packet);
+		while (isListening) {
+			try {
+				if (!socket.isClosed())
+					socket.receive(packet);
 				OSCPacket oscPacket = converter.convert(buffer, packet.getLength());
 				dispatcher.dispatchPacket(oscPacket);
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
